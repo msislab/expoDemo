cfa822032c2ef3b2a951b6ddc3c573d4cfeb14c9
A2Dexter <ammadkaisi@gmail.com>
2025-05-12 13:20:16 +0900

    Gripper and RobotG working
    added wait so new command is not send when its moving.
    update linear pcb home 1 and 2. added joints points for move_j

--- main.py
+++ main.py
@@ -14,20 +14,29 @@ ROBOT_IP_S = "192.168.0.21"
 fatal_error = False;
 error_msg = "No Error"
 
+jaws_FC = 0
+jaws_HC = 38
+jaws_HO = 50
+jaws_FO = 100
 
-robotG_speed = 0.3 # 0 to 1
-robotS_speed = 0.3 # 0 to 1
+local_speed = 100
+local_acc = 100
+
+robot_speed = 0.3 # 0 to 1
 
 pcb_safe_height = 0
 gripper_safe_height = 0
 
-robotG_home = np.array([700, 45, 430 , 90, 0, 0])
-robotS_home = np.array([250, -30, 380 , 90, 0, 90])
+robotG_home_j = np.array([175.39, -18.47, -90.83, 19.30, -89.94, 275.53])
+robotS_home = np.array([250, -30, 380, 90, 0, 90])
 
-pcb1home = np.array([984.85, -52.51, 290.21 +gripper_safe_height, 90, -0.5, -1.8])
-pcb2home = np.array([986.74, 47.30, 306.56 +gripper_safe_height, 90, 0, -1.1])
+pcb1home_j = np.array([170.50, -42.57, -69.27, 21.83, -89.96, 279.93 ])
+pcb1home = np.array([984.85, -52.51, 290.21, 90, -0.5, -1.8])
 
-PickPosition = np.array([575.406, 24.9550, 263.38 +pcb_safe_height, 90, 0, 90])
+pcb2home_j = np.array([176.31, -42.34, -69.03, 21.37, -89.95, 274.62 ])
+pcb2home = np.array([986.75, 47.30, 292.13, 90, 0, -1.1])
+
+PickPosition = np.array([575.406, 24.9550, 263.38 , 90, 0, 90])
 
 #screwing locations from pcb1
 P1 = np.array([575.406, 24.9550, 263.38 +pcb_safe_height, 90, 0, 90])
@@ -40,6 +49,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def gripper_pos(robot,rc):
+    return round(robot.get_system_variable(rc, rb.SystemVariable.JRT_JEGB)[1])
+
 def gripper_init(robot,rc):
     force = 5
     speed = 100
@@ -47,13 +59,15 @@ def gripper_init(robot,rc):
     try:
         script = f"gripper_macro 6,2,0,0,0,0,0,0,0,0"
         robot.eval(rc, script, True)
-        time.sleep(10)
-        #script = f"gripper_macro 6,2,1,0,force,speed,acc,0,0,0"
-        #robot.eval(rc, script, True)
-
-        script = f"gripper_macro 6,2,2,0,{str(100)},0,0,0,0,0"
+        time.sleep(7)
+        script = f"gripper_macro 6,2,1,0,5,100,100,0,0,0"
         robot.eval(rc, script, True)
-        print(time.sleep(10))
+
+        script = f"gripper_macro 6,2,2,0,{str(50)},0,0,0,0,0"
+        robot.eval(rc, script, True)
+        while (gripper_pos(robot, rc) != 50):
+            time.sleep(0.5)
+
         #if ( gripper_pos(robot,rc)-100) > 1:
         #    error_msg = "Gripper Error, Commanded and Feedback position not Equal"
         #    print(error_msg)
@@ -78,84 +92,118 @@ def gripper_move(robot,rc, pos):
     finally:
         pass
 
-def gripper_pos(robot,rc):
-
-    return round(robot.get_system_variable(rc, rb.SystemVariable.JRT_JEGB)[1])
 
 
 def robotG_move_home(robotG, rc):
     # move robot to home.
 
-    robotG.move_l(rc,robotG_home,100,100)
+    robotG.move_j(rc,robotG_home_j,local_speed,local_acc)
     rc.error().throw_if_not_empty()
     if robotG.wait_for_move_started(rc, 0.1).type() == rb.ReturnType.Success:
         robotG.wait_for_move_finished(rc)
     rc.error().throw_if_not_empty()
 
-def robotG_move_pcb1(robot, rc):
-    #set safe height for robot approach using j motion
-    pcb_safe_height= 20
-    robot.move_l(rc,pcb1home,200,400)
-    if robot.wait_for_move_started(rc, 0.1).type() == rb.ReturnType.Success:
-        robot.wait_for_move_finished(rc)
-    rc.error().throw_if_not_empty()
+def robotG_move_pcb1(robot, rc, pickOrDrop):
+
+    if pickOrDrop ==0:
+        gripper_move(robot, rc, jaws_HO)
+        while (gripper_pos(robot, rc) != jaws_HO):
+            time.sleep(0.5)
+        print("Jaws Position: "+str(gripper_pos(robot, rc)))
+
+    robot.move_j(rc,pcb1home_j,local_speed,local_acc)
+    time.sleep(0.25)
+    while robot.get_robot_state(rc)[1] == rb.RobotState.Moving:
+        pass
+    print("Arrived at pcb1home+20mm")
 
     #approach the pcb
-    pcb_safe_height = 0
-    robot.move_l(rc,pcb1home,200,400)
-    if robot.wait_for_move_started(rc, 0.1).type() == rb.ReturnType.Success:
-        robot.wait_for_move_finished(rc)
-    rc.error().throw_if_not_empty()
+    robot.move_l(rc,pcb1home,local_speed,local_acc)
+    time.sleep(0.25)
+    while robot.get_robot_state(rc)[1] == rb.RobotState.Moving:
+        time.sleep(0.5)
+    print("Arrived at pcb1home")
 
-    #Gripper jaws to 40 %.
-    gripper_move(robot,rc,40)
-    while(gripper_pos(robot,rc)!=40):
-        time.sleep(1)
+    if pickOrDrop == 0:
+        #Gripper jaws to CLOSE %.
+        gripper_move(robot,rc,jaws_HC)
+        time.sleep(0.25)
+        while(gripper_pos(robot,rc)!=jaws_HC):
+            time.sleep(0.5)
+        print("Jaws Position: "+str(gripper_pos(robot, rc)))
+    elif pickOrDrop == 1:
+        gripper_move(robot, rc, jaws_HO)
+        time.sleep(0.25)
+        while (gripper_pos(robot, rc) != jaws_HO):
+            time.sleep(0.5)
+    print("Jaws position: "+str(gripper_pos(robot, rc)))
 
-    pcb_safe_height = 20
-    robot.move_l(rc,pcb1home,200,400)
-    if robot.wait_for_move_started(rc, 0.1).type() == rb.ReturnType.Success:
-        robot.wait_for_move_finished(rc)
-    rc.error().throw_if_not_empty()
+    #add safe moving height
+    pcb1home[2] = pcb1home[2] +20
+    robot.move_l(rc,pcb1home,local_speed,local_acc)
+    time.sleep(0.25)
+    while robot.get_robot_state(rc)[1] == rb.RobotState.Moving:
+        time.sleep(0.5)
+    print("Arrived at pcb1home+20mm")
+    #revert by to original
+    pcb1home[2] = pcb1home[2] - 20
 
 
-def robotG_move_pcb2(robot, rc):
-    # set safe height for robot approach using j motion
-    pcb_safe_height = 20
-    robot.move_l(rc,pcb2home,200,400)
-    rc.error().throw_if_not_empty()
-    if robot.wait_for_move_started(rc, 0.1).type() == rb.ReturnType.Success:
-        robot.wait_for_move_finished(rc)
+def robotG_move_pcb2(robot, rc, pickOrDrop):
 
+    if pickOrDrop ==0:
+        # Gripper jaws to CLOSE %.
+        gripper_move(robot, rc, jaws_HO)
+        while (gripper_pos(robot, rc) != jaws_HO):
+            time.sleep(0.5)
+        print("Jaws position: "+str(gripper_pos(robot, rc)))
+
+    #pcb2home_j is safe pcb approach
+    robot.move_j(rc,pcb2home_j,local_speed,local_acc)
+    while robot.get_robot_state(rc)[1] == rb.RobotState.Moving:
+        time.sleep(0.5)
+    print("Arrived at pcb2home +20mm")
 
     # approach the pcb
-    pcb_safe_height = 0
-    robot.move_l(rc,pcb2home,200,400)
-    if robot.wait_for_move_started(rc, 0.1).type() == rb.ReturnType.Success:
-        robot.wait_for_move_finished(rc)
-    rc.error().throw_if_not_empty()
+    robot.move_l(rc,pcb2home,local_speed,local_acc)
+    while robot.get_robot_state(rc)[1] == rb.RobotState.Moving:
+        time.sleep(0.5)
+    print("Arrived at pcb2home")
 
-    # Gripper jaws to 40 %.
-    gripper_move(robot, rc, 50)
-    while (gripper_pos(robot, rc) != 50):
-        time.sleep(1)
+    if pickOrDrop == 0:
+        # Gripper jaws to CLOSE %.
+        gripper_move(robot, rc, jaws_HC)
+        while (gripper_pos(robot, rc) != jaws_HC):
+            time.sleep(0.5)
+        print("Jaws Position: "+str(gripper_pos(robot, rc)))
+    elif pickOrDrop == 1:
+        gripper_move(robot, rc, jaws_HO)
+        while (gripper_pos(robot, rc) != jaws_HO):
+            time.sleep(0.5)
+        print("Jaws Position: "+str(gripper_pos(robot, rc)))
 
+    pcb2home[2] = pcb2home[2] + 20
+    robot.move_l(rc, pcb2home, local_speed, local_acc)
+    while robot.get_robot_state(rc)[1] == rb.RobotState.Moving:
+        time.sleep(0.5)
+    print("Arrived at pcb2home +20mm")
+    pcb2home[2] = pcb2home[2] - 20
 
 def _main():
     try:
 
         #initialize cobots
         robotG = rb.Cobot(ROBOT_IP_G)
-        # robotS = rb.Cobot(ROBOT_IP_S)
+        #robotS = rb.Cobot(ROBOT_IP_S)
 
         rc = rb.ResponseCollector()
 
         robotG.set_operation_mode(rc, rb.OperationMode.Real)
-        robotG.set_speed_bar(rc, 0.2) #30% speed
+        robotG.set_speed_bar(rc, robot_speed) #30% speed
 
 
         #robotS.set_operation_mode(rc, rb.OperationMode.Real)
-        #robotS.set_speed_bar(rc, 0.3) #30% speed
+        #robotS.set_speed_bar(rc, robot_speed) #30% speed
 
         gripper_init(robotG, rc)
 
@@ -166,9 +214,12 @@ def _main():
         robotG_move_home(robotG,rc)
         print("arrived at home")
 
-        robotG_move_pcb1(robotG,rc)
+        robotG_move_pcb1(robotG,rc,0)
 
-        robotG_move_pcb2(robotG,rc)
+        robotG_move_pcb2(robotG,rc,1)
+
+        robotG_move_pcb2(robotG, rc, 0)
+        robotG_move_pcb1(robotG, rc, 1)
 
         robotG_move_home(robotG, rc)
 
