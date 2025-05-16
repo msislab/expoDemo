a75bd76705533aace93f6de9453793ba8251e290
A2Dexter <69054925+A2Dexter@users.noreply.github.com>
2025-05-16 22:16:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+MAX_Linear_Speed = 1200 #2500
+MAX_Linear_Acceleration = 2500 #1300 #2500
+MAX_Joint_Speed = 1200
+MAX_Joint_Acceleration = 1300
 
 import numpy as np
 import rbpodo as rb
